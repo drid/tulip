91bc4800aaa39780cf16d05d5855abdb3befa3d5
Ilias Daradimos <judgedrid@gmail.com>
2025-05-30 02:19:41 +0300

    feat(glyphs): add distance and dune level glyphs
    - 25m to 400m
    - distance arrow
    - Dune level 1,2,3
    
    Signed-off-by: Ilias Daradimos <judgedrid@gmail.com>

--- utils/mkglyphs.sh
+++ utils/mkglyphs.sh
@@ -6,3 +6,10 @@ do
     cat src/svg/template-speed-limit.svg| sed "s/>SPL</>$sl</" > src/svg/glyphs/speed-$sl.svg
     cat src/svg/template-speed-limit-end.svg| sed "s/>SPL</>$sl</" > src/svg/glyphs/speed-$sl-end.svg
 done
+
+echo "Generating distances"
+for d in {25..400..25}
+do 
+    echo "Distance $d"
+    cat src/svg/template-distance.svg| sed "s/>999m</>$d</" > src/svg/glyphs/distance$d.svg
+done
