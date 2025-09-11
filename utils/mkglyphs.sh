df0446392ecdcb28345c2cc0cee7731abc19565a
Ilias Daradimos <judgedrid@gmail.com>
2025-09-11 18:05:03 +0300

    chore: Added script to autogenerate abbreviations
    Signed-off-by: Ilias Daradimos <judgedrid@gmail.com>

--- utils/mkglyphs.sh
+++ utils/mkglyphs.sh
@@ -13,3 +13,12 @@ do
     echo "Distance $d"
     cat src/svg/template-distance.svg| sed "s/>999m</>$d</" > src/svg/glyphs/distance$d.svg
 done
+
+echo "Generating abbreviations"
+for abbr in "P" "PP" "RO" "P\/\/" "HP" "FPP" "FRO" "SA" "GV" "DN" "DNX" "DNT" "L1" "L2" "L3" "DSS" "ASS" "VG" "A" "ALT" "BTW" "DNX" "IN" "kpL" "kpR" "kpS" "L\/R" "onL" "onR" "R\/L" "RP" "CX" "TJS" "NBX" "IMP" "EFF" "ORN" "BAD" "RP"
+do 
+    fname="src/svg/glyphs/abbr-"$(echo $abbr | tr '/' '-' | tr -d '\\')".svg"
+    echo "Abbreviation $abbr -> $fname"
+    cc=$(echo -n $abbr | tr -d '\\' | wc -m)
+    cat src/svg/template-abbr$cc.svg| sed "s/>BTW</>$abbr</" > $fname
+done
