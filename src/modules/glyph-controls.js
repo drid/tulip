b1afcbf5a8723ba449a8da8664127064a202c484
Ilias Daradimos <judgedrid@gmail.com>
2025-09-11 18:53:27 +0300

    refactor: Glyphs are loaded from JSON file, glyph search uses description text
    Signed-off-by: Ilias Daradimos <judgedrid@gmail.com>

--- src/modules/glyph-controls.js
+++ src/modules/glyph-controls.js
@@ -1,24 +1,15 @@
 // TODO refactor this to use MVC pattern and act as a controller for the currentlyEditingInstruction for the roadbook
 class GlyphControls {
 
-  constructor(app_path) {
-    this.files = [];
+  constructor(app_path, glyphStructure) {
     this.glyph_path = app_path + '/assets/svg/glyphs/';
-    this.getGylphNames();
+    this.glyphStructure = glyphStructure
+    this.populateGlyphs();
     this.initListeners();
     this.bindToGlyphImages();
     this.addToNote = false;
   }
-
-  getGylphNames() {
-    try {
-      this.files = globalNode.fs.readdirSync(this.glyph_path).filter(function (val) { return val.endsWith('.svg') });
-    } catch (e) {
-      console.log("using unpackaged filesys");
-      this.files = globalNode.fs.readdirSync('assets/svg/glyphs').filter(function (val) { return val.endsWith('.svg') });
-    }
-  }
-
+  
   handleGlyphSelectUI(e) {
     e.preventDefault();
     if (!e.shiftKey) {
@@ -31,8 +22,8 @@ class GlyphControls {
     var _this = this;
     $.each(results, function (i, result) {
       // Check for waypoint or control icons
-      var img = $('<img>').addClass('glyph').attr('src', result.path)
-      var link = $('<a>').addClass('th').attr('title', result.name).append(img);
+      var img = $('<img>').addClass('glyph').attr('src', result.src)
+      var link = $('<a>').addClass('th').attr('title', result.name).append(img).append($('<p>').text(result.text));
       var showResult = $('<li>').append(link);
       $(img).click(function (e) {
         _this.handleGlyphSelectUI(e);
@@ -43,94 +34,158 @@ class GlyphControls {
   }
 
   searchGlyphNames(query) {
-    console.log("searching for " + query);
-    query = query.toLowerCase();
-    var results = [];
-    $.each(this.files, function (i, file) {
-      var fname = file.toLowerCase();
-      if (fname.indexOf(query) != -1) {
-        results.push({ name: file.replace('.svg', ''), path: 'assets/svg/glyphs/' + file })
+    const results = [];
+    this.glyphStructure.forEach(section => {
+      if (section.items) {
+        section.items.forEach(item => {
+          if (item.text.toLowerCase().includes(query.toLowerCase())) {
+            results.push({ src: item.src, text: item.text });
+          }
+        });
+      } else if (section.tabs) {
+        section.tabs.forEach(tab => {
+          tab.items.forEach(item => {
+            if (item.text.toLowerCase().includes(query.toLowerCase())) {
+              results.push({ src: item.src, text: item.text });
+            }
+          });
+        });
       }
     });
     return results;
   }
 
-  bindToGlyphImages() {
-    var _this = this;
-    $('.glyph').click(function (e) {
-      _this.handleGlyphSelectUI(e);
-      _this.addGlyphToInstruction(this);
-      app.noteControls.checkForNotification();
-    });
-  }
+bindToGlyphImages() {
+  var _this = this;
+  $('.glyph').click(function (e) {
+    _this.handleGlyphSelectUI(e);
+    _this.addGlyphToInstruction(this);
+    app.noteControls.checkForNotification();
+  });
+}
 
-  initListeners() {
-    var _this = this;
-    $('#glyph-search').keyup(function () {
-      $('#glyph-search-results').html('');
-      if ($(this).val() != '') {
-        var results = _this.searchGlyphNames($(this).val());
-        _this.populateResults(results);
-        $('.glyph').off('click')
-        _this.bindToGlyphImages();
-      }
-    });
+initListeners() {
+  var _this = this;
+  $('#glyph-search').keyup(function () {
+    $('#glyph-search-results').html('');
+    if ($(this).val() != '') {
+      var results = _this.searchGlyphNames($(this).val());
+      _this.populateResults(results);
+      $('.glyph').off('click')
+      _this.bindToGlyphImages();
+    }
+  });
 
-    $('#glyph-search-clear').click(function () {
-      $('#glyph-search').val('');
-      $('#glyph-search-results').html('');
-      $('#glyph-search').focus();
-    })
+  $('#glyph-search-clear').click(function () {
+    $('#glyph-search').val('');
+    $('#glyph-search-results').html('');
+    $('#glyph-search').focus();
+  })
 
-    $('.note-grid').click(function (e) {
-      e.preventDefault();
-      _this.addToNote = true;
-      $('#glyphs').foundation('reveal', 'open');
-      setTimeout(function () { $('#glyph-search').focus(); }, 600); //we have to wait for the modal to be visible before we can assign focus
-    });
-
-    //TODO fill out this todo, you know you wanna.
-    $('.glyph-grid').click(function (e) {
-      e.preventDefault();
-      if ($(this).hasClass('undo')) {
-        app.roadbook.currentlyEditingInstruction.tulip.removeLastGlyph();
-        return false
-      }
-      _this.showGlyphModal($(this).data('top'), $(this).data('left'));
-      return false
-    });
-
-    $(document).on('keydown', function (e) {
-      if ((e.key === 'Delete' || e.key === 'Backspace') && !$(e.target).is('input, textarea') && app.roadbook.currentlyEditingInstruction) {
-        app.roadbook.currentlyEditingInstruction.tulip.removeActiveGlyph()
-      }
-    });
-  }
-
-  showGlyphModal(top, left) {
-    app.glyphPlacementPosition = { top: top, left: left };
-    this.addToNote = false;
+  $('.note-grid').click(function (e) {
+    e.preventDefault();
+    _this.addToNote = true;
     $('#glyphs').foundation('reveal', 'open');
     setTimeout(function () { $('#glyph-search').focus(); }, 600); //we have to wait for the modal to be visible before we can assign focus
+  });
+
+  //TODO fill out this todo, you know you wanna.
+  $('.glyph-grid').click(function (e) {
+    e.preventDefault();
+    if ($(this).hasClass('undo')) {
+      app.roadbook.currentlyEditingInstruction.tulip.removeLastGlyph();
+      return false
+    }
+    _this.showGlyphModal($(this).data('top'), $(this).data('left'));
     return false
+  });
+
+  $(document).on('keydown', function (e) {
+    if ((e.key === 'Delete' || e.key === 'Backspace') && !$(e.target).is('input, textarea') && app.roadbook.currentlyEditingInstruction) {
+      app.roadbook.currentlyEditingInstruction.tulip.removeActiveGlyph()
+    }
+  });
+}
+
+showGlyphModal(top, left) {
+  app.glyphPlacementPosition = { top: top, left: left };
+  this.addToNote = false;
+  $('#glyphs').foundation('reveal', 'open');
+  setTimeout(function () { $('#glyph-search').focus(); }, 600); //we have to wait for the modal to be visible before we can assign focus
+  return false
+}
+
+addGlyphToInstruction(element) {
+  var src = $(element).attr('src');
+  var filename = src.substring(src.lastIndexOf('/') + 1);
+  var glyph = filename.replace('.svg', '')
+  if (Notification.mapFileNameToType(glyph) != undefined) {
+    app.roadbook.currentlyEditingInstruction.manageWaypoint(glyph);
+    return;
+  }
+  if (this.addToNote) {
+    // NOTE this module should only know about the roadbook
+    app.roadbook.appendGlyphToNoteTextEditor($('<img>').attr('src', src).addClass('normal'));
+  } else {
+    // NOTE this module should only know about the roadbook
+    app.roadbook.currentlyEditingInstruction.tulip.addGlyph(app.glyphPlacementPosition, src);
   }
 
-  addGlyphToInstruction(element) {
-    var src = $(element).attr('src');
-    var filename = src.substring(src.lastIndexOf('/') + 1);
-    var glyph = filename.replace('.svg', '')
-    if (Notification.mapFileNameToType(glyph) != undefined) {
-      app.roadbook.currentlyEditingInstruction.manageWaypoint(glyph);
-      return;
-    }
-    if (this.addToNote) {
-      // NOTE this module should only know about the roadbook
-      app.roadbook.appendGlyphToNoteTextEditor($('<img>').attr('src', src).addClass('normal'));
-    } else {
-      // NOTE this module should only know about the roadbook
-      app.roadbook.currentlyEditingInstruction.tulip.addGlyph(app.glyphPlacementPosition, src);
-    }
+}
 
-  }
+populateGlyphs() {
+  const accordion = this._generateAccordion(this.glyphStructure);
+  $('#glyph-content').html(accordion);
+}
 
+// Function to generate HTML for items
+_generateItems(items) {
+  return items.map(item => `
+    <li><a class="th" href="#"><img class="glyph" src="${item.src}"></a><p>${item.text}</p></li>
+  `).join('');
+}
+
+// Function to generate HTML for tabs
+_generateTabs(tabs) {
+  const tabTitles = tabs.map(tab => `
+    <li class="tab-title${tab.active ? ' active' : ''}"><a href="#${tab.id}">${tab.title}</a></li>
+  `).join('');
+
+  const tabContents = tabs.map(tab => `
+    <div class="content${tab.active ? ' active' : ''}" id="${tab.id}">
+      <ul class="small-block-grid-5">
+        ${this._generateItems(tab.items)}
+      </ul>
+    </div>
+  `).join('');
+
+  return `
+    <ul class="tabs" data-tab>
+      ${tabTitles}
+    </ul>
+    <div class="tabs-content">
+      ${tabContents}
+    </div>
+    `;
+}
+
+// Function to generate HTML for accordion sections
+_generateAccordion(data) {
+  return `
+    <ul class="accordion" data-accordion>
+      ${data.map(section => `
+        <li class="accordion-navigation">
+          <a href="#${section.id}">${section.title}</a>
+          <div id="${section.id}" class="content">
+            ${section.tabs ? this._generateTabs(section.tabs) : `
+              <ul class="small-block-grid-5">
+                ${this._generateItems(section.items)}
+              </ul>
+            `}
+          </div>
+        </li>
+      `).join('')}
+    </ul>
+  `;
+}
 };
