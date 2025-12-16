bea018190e210a08b82e228d5b0dbd43103866bf
Ilias Daradimos <judgedrid@gmail.com>
2025-12-16 20:58:32 +0200

    refactor:  Track type options
    - Simplify track options
    - Remove redundant code from tulip.js
    - Update tests
    
    Signed-off-by: Ilias Daradimos <judgedrid@gmail.com>

--- src/modules/tulip.js
+++ src/modules/tulip.js
@@ -30,7 +30,6 @@ var Tulip = Class({
     this.addedTrackType = 'track';
     // TODO should this be checking JSON for this?
     this.exitTrackEdited = false;
-    this.mainTrackColor = '#22F';
     this.markerAngle = 45;
     this.initTulip(angle, trackTypes, json);
   },
@@ -257,14 +256,6 @@ var Tulip = Class({
   },
 
   buildEntryTrackFromJson(entry, entryTrackType) {
-    entry.paths = entry.paths.map(obj => {
-      if (obj.stroke === '#000') {
-        return { ...obj, stroke: this.mainTrackColor };
-      }
-      return obj;
-    });
-    entry.point.stroke = this.mainTrackColor;
-    entry.point.fill = this.mainTrackColor;
     entry.point.selectable = false;
     var paths = this.buildPaths(entry.paths);
     var point = new fabric.Circle(entry.point);
@@ -273,14 +264,6 @@ var Tulip = Class({
   },
 
   buildExitTrackFromJson(exit, exitTrackType) {
-    exit.paths = exit.paths.map(obj => {
-      if (obj.stroke === '#000') {
-        return { ...obj, stroke: this.mainTrackColor };
-      }
-      return obj;
-    });
-    exit.point.stroke = this.mainTrackColor;
-    exit.point.fill = this.mainTrackColor;
     var paths = this.buildPaths(exit.paths);
     var point = new fabric.Triangle(exit.point)
     this.canvas.add(point);
